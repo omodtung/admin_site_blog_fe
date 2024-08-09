ef7d33bf307599e43a878ce06756f22c982d70b2
omodtung <dothedung131@gmail.com>
2024-08-09 18:08:00 +0700

    Count Total Number Of User On DashBoard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,25 @@
 import React from "react";
-
+import { useEffect, useState } from "react";
+import requestApi from "../helpers/api";
 const Dashboard = () => {
+  const [dashboardData, setDashboardData] = useState({});
+// dashboardData là một đối tượng JavaScript.
+// totalUser là một thuộc tính (property) của đối tượng dashboardData.
+// Việc sử dụng toán tử spread (...) trong setDashboardData đảm bảo rằng chúng ta không ghi đè lên các thuộc tính khác trong dashboardData mà chỉ thêm thuộc tính mới vào.
+  useEffect(() => {
+    requestApi("/user", "GET", [])
+      .then((response) => {
+        console.log(response);
+        setDashboardData({
+          ...dashboardData,
+          totalUser: response.data.total,
+        });
+      })
+
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div id="layoutSidenav_content">
       <main>
@@ -12,14 +31,13 @@ const Dashboard = () => {
           <div className="row">
             <div className="col-xl-3 col-md-6">
               <div className="card bg-primary text-white mb-4">
-                <div className="card-body">Total Users</div>
-                <div className="card-footer d-flex align-items-center justify-content-between">
-                  <a className="small text-white stretched-link" href="#">
-                    View Details
-                  </a>
-                  <div className="small text-white">
-                    <i className="fas fa-angle-right"></i>
-                  </div>
+                <div className="card-body">
+                  Total Users
+                  {dashboardData.totalUser && (
+                    <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                      {dashboardData.totalUser}
+                    </span>
+                  )}
                 </div>
               </div>
             </div>
