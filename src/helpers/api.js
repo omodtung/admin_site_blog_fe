4bb68206c0513b3c5fab6ea49351a5e18f605c66
omodtung <dothedung131@gmail.com>
2024-08-09 18:07:29 +0700

    write Method For Access Token and ReFresh Token

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -6,11 +6,72 @@ export default function requestApi(
   responseType = "json"
 ) {
   const headers = {
-    "Accept": "application/json",
+    Accept: "application/json",
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": "*",
   };
   const instance = axios.create({ headers });
+
+  // gui Reqest len NestJS
+  //khi luc moi vo thi dung lau access_token nay de vo
+
+  //
+  //   // In summary, this code intercepts outgoing requests and adds an "Authorization" header to the request if a valid access token exists in local storage. If an error occurs during the request, it rejects the promise with that error.
+
+  //
+  instance.interceptors.request.use(
+    (config) => {
+      //  token này được láy ra cho mục đích Authetication
+      const token = localStorage.getItem("access_token");
+      if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+
+    async (error) => {
+      const originalConfig = error.config;
+      console.log("Access token Expired");
+      if (error.response && error.response.status === 419) {
+        //khi token het han
+        try {
+          console.log("call refresh token api");
+          // goi len endpoint fresh_token
+          const result = await instance.post(
+            `${process.env.REACT_APP_API_URL}/auth/refresh-token`,
+            {
+              // lay token tu local storge luu tu truoc
+              refresh_token: localStorage.getItem("refresh_token"),
+            }
+          );
+
+          const { access_token, refresh_token } = result.data;
+          //set AccessToken va Refresh TOken moi vao local- storage
+          localStorage.setItem("access_token", access_token);
+          localStorage.setItem("refresh_token", refresh_token);
+          originalConfig.headers["Authorization"] = `Bearer ${access_token}`;
+          return instance(originalConfig);
+        } catch (err) {
+          // xay ra loi thi bo xoa token  roi bo luon
+          if (err.response && err.response.status === 400) {
+            localStorage.removeItem("access_token");
+            localStorage.removeItem("refresh_token");
+            window.location.href = "/login";
+          }
+          return Promise.reject(err);
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
 
   return instance.request({
     method: method,
